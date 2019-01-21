618aedb6782620674a5a22edbaa1908536eecd38
Jarret Dyrbye <jarret.dyrbye@gmail.com>
2019-01-20 21:01:35 -0700

    examples

--- eink_test.py
+++ eink_test.py
@@ -18,9 +18,24 @@ from waveshare.epaper import ReadBaudrate
 from qrdraw import QRDraw
 
 import time
+import random
 
 
-MOCK_BOLT11 = "lnbc50n1pdm373mpp50hlcjdrcm9u3qqqs4a926g63d3t5qwyndytqjjgknskuvmd9kc2sdz2d4shyapwwpujq6twwehkjcm9ypnx7u3qxys8q6tcv4k8xtpqw4ek2ujlwd68y6twvuazqg3zyqxqzjcuvzstexcj4zcz7ldtkwz8t5pdsghauyhkdqdxccx8ts3ta023xqzwgwxuvlu9eehh97d0qcu9k5a4u2glenrekp7w9sswydl4hneyjqqzkxf54"
+SELECTIONS = [
+    {'first_line':  "Coca Cola",
+     'second_line': "The Great National Temperance Beverage",
+     'price':       123.33,
+     'invoice':     "lnbc1233330p1pwy2sjlpp5cg2n6p2604ljsjefv3ywz0ree09m6d24vngshf9u5t0jmq3jf80sdzsgdhkxcfqgdhkccfq95s9g6r9yprhyetpwssyuct5d9hkuctvyp2x2mtsv4exzmnrv5syyetkv4exzem9cqp2rzjqfffmd5l6t4axyn0keh6lg35ls65g3m6y02snl5na53fhv8f9e8mszpg8sqqwhsqqyqqqqqfqqqqqfcqjqktpykty5j5m7n3vw926qt69fjdvrye4vzfwlquh6tvezv85q7trqmma9tnngjf570tj5975nvshlf4rdq77f2wjgdg34sf9dmt3fxxspctr0nk"},
+    {'first_line':  "Pepsi Cola",
+     'second_line': "You Got the Right One Baby",
+     'price':       324.340,
+     'invoice':     "lnbc3243400p1pwy2s48pp5zgwzlvv2ndknql8g727wetpg9wlexxsq6fhh9d6vmdc2yfylt4tqdpl2pjhqumfyppk7mrpyqkjqkt0w5sywmm5yp6xsefq2f5kw6r5yp8kuefqgfsky7gcqp2rzjqtdg6kn4nm57gsud5ctulkmpeplhy0ahd39kxudcwlgrg747j5ay7zz2wsqqzrqqqyqqqq86qqqqqqgqjq3kklhw68qdglk7kqmsnm3r484zme9k6ctuaq9449htcvzt7fvchqjx5h0ceyzryfxp0af9m5k6vk0e7wp6ntxl9krjlcpcp39mx96vgq9extkc"},
+    {'first_line':  "Mountain Dew Code Red",
+     'second_line': "Discover A Sensation As Real As The Streets",
+     'price':       555.555,
+     'invoice':     "lnbc5555550p1pwy2skkpp5eycqq2dl4twg6y0yqafpvh56chuckhp3npctw272ph7ju2df9azsdrvf4hh2mn5v95kugzyv4mjqsm0v3jjq5n9vssz6gzyd9ekxmmkv4ezqsfq2djkuumpw35k7m3qg9ejq5n9v9kzqstnyp2xsefq2d68yet9w3escqp2rzjqtdg6kn4nm57gsud5ctulkmpeplhy0ahd39kxudcwlgrg747j5ay7zz2wsqqzrqqqyqqqq86qqqqqqgqjq4fhfwjetqhg0ccpn23lss28gs8l0jqf4g3w88l7ml4679hkdzqy9m502wl5zugkqv6cqrpsh9aenrf0p4l5mzncsaa0hua8t8k86sgspamzccq"},
+]
+
 
 class Screen(object):
     def __init__(self, paper):
@@ -124,9 +139,10 @@ class Screen(object):
 
 
 if __name__ == '__main__':
-    d = QRDraw(MOCK_BOLT11)
-    line1 = "Never Gonna Give You Up!"
-    line2 = "Never Gonna Let You Down!"
+    choice = random.choice(SELECTIONS)
+    d = QRDraw(choice['invoice'])
+    line1 = choice['first_line']
+    line2 = choice['second_line']
     print('\n'.join(d.iter_string_rows()))
 
     with EPaper() as paper:
